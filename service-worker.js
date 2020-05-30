0e705d13943d82dcf4c3c9f4493f8a0b0d06c6c8
=Rozbytskyi <rozbich15@gmail.com>
2020-05-30 16:27:23 +0200

    fixing responsiveness on the track perspective

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02f16505da2b0bb56c2590eb95b1f216.js"
+  "/precache-manifest.957176283bfac93a71e7e4a2d15c14f8.js"
 );
 
 self.addEventListener('message', (event) => {
